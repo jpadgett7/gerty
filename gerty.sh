6c5626387ca3c3bdda4eb28ed0e257ee3c8a50e1
Michael Wisely <michaelwisely@gmail.com>
2015-04-15 11:36:36 -0500

    Update gerty.sh for convenience

--- gerty.sh
+++ gerty.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-FILE_BRAIN_PATH=./ BIND_ADDRESS="localhost" HUBOT_AUTH_ADMIN="" bin/hubot -a shell -n gerty
+source secret.sh
+
+if [ -z "$GERTY_TOKEN" ]
+then
+    echo "Please note that Gerty cannot update the SIG-Game status site without a GitHub Token"
+else
+    export HUBOT_STATUS_GITHUB_TOKEN=$GERTY_TOKEN
+fi
+
+HUBOT_STATUS_REPO_NAME="status" \
+    HUBOT_STATUS_REPO_OWNER="siggame" \
+    FILE_BRAIN_PATH=./ \
+    BIND_ADDRESS="localhost" \
+    HUBOT_AUTH_ADMIN="1" \
+    bin/hubot -a shell -n gerty
